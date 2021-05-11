52f332614f9c6847d52c6616bd8b1542c3ec25a5
monsterkosh <mnnnkosh@gmail.com>
2021-05-11 19:32:43 -0300

    singlerooms

--- src/context.js
+++ src/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import items from './data';
 
+// @ts-ignore
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
@@ -34,9 +35,15 @@ class RoomProvider extends Component {
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRoos = [...this.state.rooms];
+    const room = tempRoos.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
