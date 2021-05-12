6d95ffdd6d1555466a1bc706cb8d89f3d0c621bc
monsterkosh <mnnnkosh@gmail.com>
2021-05-12 01:23:28 -0300

    room filters

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { Component } from 'react';
 import items from './data';
 
@@ -57,13 +58,59 @@ class RoomProvider extends Component {
   };
 
   handleChange = (event) => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
   };
 
   filterRooms = () => {
-    console.log('filter rooms function');
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
+    // all the rooms
+    let tempRooms = [...rooms];
+
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    // filter by type
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    // filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
+
+    // change state
+    this.setState({
+      sortedRooms: tempRooms,
+    });
   };
 
   render() {
